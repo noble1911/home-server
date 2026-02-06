a8b433a8558df58def5bd81e51246a5b0b483684
Ronald Noble <noble1911@gmail.com>
2026-02-06 19:44:05 +0900

    Add desktop sidebar navigation to PWA (#85) (#86)
    Closes #85
    
    Co-authored-by: Ron Noble <ron@obsidianos.com>

--- app/src/components/layout/Layout.tsx
+++ app/src/components/layout/Layout.tsx
@@ -1,14 +1,18 @@
 import { Outlet } from 'react-router-dom'
 import Header from './Header'
 import BottomNav from './BottomNav'
+import Sidebar from './Sidebar'
 
 export default function Layout() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex-1 overflow-y-auto pb-20 md:pb-0">
-        <Outlet />
-      </main>
+      <div className="flex flex-1 overflow-hidden">
+        <Sidebar />
+        <main className="flex-1 overflow-y-auto pb-20 md:pb-0">
+          <Outlet />
+        </main>
+      </div>
       <BottomNav />
     </div>
   )
